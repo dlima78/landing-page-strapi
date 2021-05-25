8d5f07f6add1addc82cf859f215bdec26e614bbf
Eduardo Lima <dlima78@hotmail.com>
2021-05-25 11:51:25 -0300

    feat: add logo

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,13 @@
+import { gql } from 'graphql-request'
+
+const GET_LANDING_PAGE = gql`
+  query GET_LANDING_PAGE {
+    landingPage {
+      logo {
+        alternativeText
+        url
+      }
+    }
+  }
+`
+export default GET_LANDING_PAGE
